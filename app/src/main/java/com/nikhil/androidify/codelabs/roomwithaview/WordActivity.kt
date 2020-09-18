aea3327dfe04517938e57267142a22d647e6db60
Nikhil <nikhilmht33@gmail.com>
2020-09-18 21:22:55 +0530

    Add WordActivity and related XMLs

---
+++ app/src/main/java/com/nikhil/androidify/codelabs/roomwithaview/WordActivity.kt
@@ -0,0 +1,13 @@
+package com.nikhil.androidify.codelabs.roomwithaview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.nikhil.androidify.R
+
+class WordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_word)
+    }
+}
