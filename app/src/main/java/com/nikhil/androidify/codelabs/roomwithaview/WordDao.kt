5fc5191389bc5a801c2e6532ff8a0aba566c9083
Nikhil <nikhilmht33@gmail.com>
2020-09-18 10:10:23 +0530

    Add Room Dao

---
+++ app/src/main/java/com/nikhil/androidify/codelabs/roomwithaview/WordDao.kt
@@ -0,0 +1,42 @@
+package com.nikhil.androidify.codelabs.roomwithaview
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+/**
+ * In the DAO (data access object), you specify SQL queries and associate them with method calls.
+ * The compiler checks the SQL and generates queries from convenience annotations for common queries, such as @Insert.
+ *
+ * The DAO must be an interface or abstract class.
+ *
+ * By default, all queries must be executed on a separate thread.
+ *
+ * Room has coroutines support, allowing your queries to be annotated with the suspend modifier and
+ * then called from a coroutine or from another suspension function.
+ */
+@Dao
+interface WordDao {
+
+    /**
+     * Getting all words ordered alphabetically
+     * [Query] requires that you provide a SQL query as a string parameter to the annotation, allowing for complex read queries and other operations.
+     */
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    fun getAlphabetizedWords(): List<Word>
+
+    /**
+     * Declares a [suspend] function to insert one word
+     * The selected onConflict strategy ignores a new word if it's exactly the same as one already in the list.
+     */
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    /**
+     * Declares a [suspend] function to delete all the  words.
+     * There is no convenience annotation for deleting multiple entities, so it's annotated with the generic [Query]
+     */
+    @Query("DELETE FROM word_table")
+    suspend fun deleteAll()
+}
