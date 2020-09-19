7052583110d21f2146fd2418884bd9d2be9132d5
Nikhil <nikhilmht33@gmail.com>
2020-09-19 12:32:22 +0530

    Create a Binding Adapter with multiple parameters

--- app/src/main/java/com/nikhil/androidify/codelabs/databinding/util/BindingAdapters.kt
+++ app/src/main/java/com/nikhil/androidify/codelabs/databinding/util/BindingAdapters.kt
@@ -1,8 +1,11 @@
 package com.nikhil.androidify.codelabs.databinding.util
 
 import android.view.View
+import android.widget.ProgressBar
 import androidx.databinding.BindingAdapter
 
+
+const val MAX_LIKES_TO_FILL_PROGRESS_BAR = 5
 /**
  * In Kotlin, static methods can be created by adding functions to the top level of a Kotlin file or as extension functions on the class.
  */
@@ -34,4 +37,23 @@ import androidx.databinding.BindingAdapter
 @BindingAdapter("app:hideIfZero")
 fun hideIfZero(view: View, number: Int) {
     view.visibility = if (number == 0) View.GONE else View.VISIBLE
+}
+
+/**
+ *  Sets the value of the progress bar so that 5 likes will fill it up.
+ *
+ *  Showcases Binding Adapters with multiple attributes.
+ *  Note that this adapter is called whenever any of the attribute changes.
+ *
+ *  This Binding Adapter is not used if any of the attributes are missing.
+ *  This happens at compile time.
+ *  The method takes 3 parameters now (the view it applies to plus the number of attributes defined in the annotation).
+ *
+ *  The requireAll parameter defines when the binding adapter is used:
+ *  1. When true, all elements must be present in the XML definition.
+ *  2. When false, the missing attributes will be null, false if booleans, or 0 if primitives.
+ */
+@BindingAdapter(value = ["app:progressScaled", "android:max"], requireAll = true)
+fun setProgress(progressBar: ProgressBar, likes: Int, max: Int) {
+    progressBar.progress = (likes * max / MAX_LIKES_TO_FILL_PROGRESS_BAR).coerceAtMost(max)
 }
