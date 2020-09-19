50f24f2aed0e562c39f26c49a8c06ed5a491a8f9
Nikhil <nikhilmht33@gmail.com>
2020-09-19 10:12:39 +0530

    Add setup for Data Binding

---
+++ app/src/main/java/com/nikhil/androidify/codelabs/databinding/BasicSampleActivity.kt
@@ -0,0 +1,18 @@
+package com.nikhil.androidify.codelabs.databinding
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * The Data Binding Library is an Android Jetpack library that allows you to bind UI components in your XML layouts to "data sources" in your app
+ * using a "declarative format" rather than programmatically, reducing boilerplate code.
+ *
+ * This CodeLab showcases following examples:
+ * 1. Static data binding
+ * 2. Observable data binding
+ * 3. Custom [srcCompat] Binding Method
+ * 4. Provided [text] Binding Adapter
+ * 5. Custom [progressTint] Binding Adapter
+ */
+class BasicSampleActivity :AppCompatActivity(){
+
+}
