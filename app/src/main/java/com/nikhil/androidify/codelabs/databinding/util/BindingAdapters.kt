2a88babf05f703991af76c684d6fccc1afeb31cb
Nikhil <nikhilmht33@gmail.com>
2020-09-19 12:50:02 +0530

    Create more Binding Adapter with multiple parameters

--- app/src/main/java/com/nikhil/androidify/codelabs/databinding/util/BindingAdapters.kt
+++ app/src/main/java/com/nikhil/androidify/codelabs/databinding/util/BindingAdapters.kt
@@ -1,9 +1,18 @@
 package com.nikhil.androidify.codelabs.databinding.util
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.view.View
+import android.widget.ImageView
 import android.widget.ProgressBar
+import androidx.core.content.ContextCompat
+import androidx.core.widget.ImageViewCompat
 import androidx.databinding.BindingAdapter
-
+import com.nikhil.androidify.R
+import com.nikhil.androidify.codelabs.databinding.Popularity
 
 const val MAX_LIKES_TO_FILL_PROGRESS_BAR = 5
 /**
@@ -56,4 +65,50 @@ fun hideIfZero(view: View, number: Int) {
 @BindingAdapter(value = ["app:progressScaled", "android:max"], requireAll = true)
 fun setProgress(progressBar: ProgressBar, likes: Int, max: Int) {
     progressBar.progress = (likes * max / MAX_LIKES_TO_FILL_PROGRESS_BAR).coerceAtMost(max)
+}
+
+/**
+ * A Binding Adapter that is called whenever the value of the attribute `app:popularityIcon` changes.
+ * Receives a popularity level that determines the icon and tint color to use.
+ */
+@BindingAdapter("app:popularityIcon")
+fun popularityIcon(view: ImageView, popularity: Popularity) {
+    val color = getAssociatedColor(popularity, view.context)
+    ImageViewCompat.setImageTintList(view, ColorStateList.valueOf(color))
+    view.setImageDrawable(getDrawablePopularity(popularity, view.context))
+}
+
+/**
+ * A Binding Adapter that is called whenever the value of the attribute `android:progressTint` changes.
+ * Depending on the value it determines the color of the progress bar.
+ */
+@SuppressLint("ObsoleteSdkInt")
+@BindingAdapter("app:progressTint")
+fun tintPopularity(view: ProgressBar, popularity: Popularity) {
+    val color = getAssociatedColor(popularity, view.context)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        view.progressTintList = ColorStateList.valueOf(color)
+    }
+}
+
+private fun getAssociatedColor(popularity: Popularity, context: Context): Int {
+    return when (popularity) {
+        Popularity.NORMAL -> context.theme.obtainStyledAttributes(intArrayOf(android.R.attr.colorForeground)).getColor(0, 0x000000)
+        Popularity.POPULAR -> ContextCompat.getColor(context, R.color.popular)
+        Popularity.STAR -> ContextCompat.getColor(context, R.color.star)
+    }
+}
+
+private fun getDrawablePopularity(popularity: Popularity, context: Context): Drawable? {
+    return when (popularity) {
+        Popularity.NORMAL -> {
+            ContextCompat.getDrawable(context, R.drawable.ic_person_black_96dp)
+        }
+        Popularity.POPULAR -> {
+            ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
+        }
+        Popularity.STAR -> {
+            ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
+        }
+    }
 }
