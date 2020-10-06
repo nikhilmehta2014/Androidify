b672e29864e0caec6563e566ef25333678f0d28c
Nikhil <nikhilmht33@gmail.com>
2020-10-06 11:18:53 +0530

    Add Advanced Coroutines activity

--- app/src/main/java/com/nikhil/androidify/codelabs/CodeLabsActivity.kt
+++ app/src/main/java/com/nikhil/androidify/codelabs/CodeLabsActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.nikhil.androidify.R
 import com.nikhil.androidify.codelabs.coroutines.main.BasicCoroutinesActivity
+import com.nikhil.androidify.codelabs.coroutinesadvanced.AdvancedCoroutinesActivity
 import com.nikhil.androidify.codelabs.databinding.basicsample.BasicSampleActivity
 import com.nikhil.androidify.codelabs.roomwithaview.WordActivity
 import kotlinx.android.synthetic.main.activity_codelabs.*
@@ -24,6 +25,9 @@ class CodeLabsActivity :AppCompatActivity(){
         }
         tv_basic_coroutines.setOnClickListener {
             startActivity(Intent(this, BasicCoroutinesActivity::class.java))
+        }
+        tv_advanced_coroutines.setOnClickListener {
+            startActivity(Intent(this, AdvancedCoroutinesActivity::class.java))
         }
     }
 }
---
+++ app/src/main/java/com/nikhil/androidify/codelabs/coroutinesadvanced/AdvancedCoroutinesActivity.kt
@@ -0,0 +1,7 @@
+package com.nikhil.androidify.codelabs.coroutinesadvanced
+
+import androidx.appcompat.app.AppCompatActivity
+
+class AdvancedCoroutinesActivity :AppCompatActivity(){
+
+}
