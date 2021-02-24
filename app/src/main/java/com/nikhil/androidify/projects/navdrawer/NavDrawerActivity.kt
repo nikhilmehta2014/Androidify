9d3f0ad404c3a4e21d1fc88e4fc71802d4290d59
Nikhil <nikhilmht33@gmail.com>
2021-02-24 18:11:29 +0530

    Add Navigation Drawer resources and logic

--- app/src/main/java/com/nikhil/androidify/projects/navdrawer/NavDrawerActivity.kt
+++ app/src/main/java/com/nikhil/androidify/projects/navdrawer/NavDrawerActivity.kt
@@ -1,6 +1,14 @@
 package com.nikhil.androidify.projects.navdrawer
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.nikhil.androidify.R
 
 class NavDrawerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav_drawer)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
 }
