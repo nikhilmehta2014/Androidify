6ccbf601739351ab380b9e2914cd7d5846334dfa
Nikhil <nikhilmht33@gmail.com>
2021-02-24 17:57:18 +0530

    Add Navigation Drawer Boilerplate code

--- app/src/main/java/com/nikhil/androidify/projects/TopicsActivity.kt
+++ app/src/main/java/com/nikhil/androidify/projects/TopicsActivity.kt
@@ -3,11 +3,11 @@ package com.nikhil.androidify.projects
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.nikhil.androidify.R
 import com.nikhil.androidify.projects.launchmodes.FirstActivity
 import com.nikhil.androidify.projects.layouts.constraintlayout.ConstraintLayoutExample1
 import com.nikhil.androidify.projects.lifecycle.LifecycleActivity
+import com.nikhil.androidify.projects.navdrawer.NavDrawerActivity
 import kotlinx.android.synthetic.main.activity_topics.*
 
 class TopicsActivity : AppCompatActivity() {
@@ -25,6 +25,9 @@ class TopicsActivity : AppCompatActivity() {
         }
         tv_constraint_layout_ex_1.setOnClickListener {
             startActivity(Intent(this, ConstraintLayoutExample1::class.java))
+        }
+        tv_nav_drawer.setOnClickListener {
+            startActivity(Intent(this, NavDrawerActivity::class.java))
         }
     }
 }
---
+++ app/src/main/java/com/nikhil/androidify/projects/navdrawer/NavDrawerActivity.kt
@@ -0,0 +1,6 @@
+package com.nikhil.androidify.projects.navdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+
+class NavDrawerActivity : AppCompatActivity() {
+}
