a46c2ea14da2c770e0f93533b4a239d2c71697e2
Nikhil <nikhilmht33@gmail.com>
2021-02-24 18:15:01 +0530

    Add ActionBarDrawerToggle and set click listener

--- app/src/main/java/com/nikhil/androidify/projects/navdrawer/NavDrawerActivity.kt
+++ app/src/main/java/com/nikhil/androidify/projects/navdrawer/NavDrawerActivity.kt
@@ -1,8 +1,12 @@
 package com.nikhil.androidify.projects.navdrawer
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import com.nikhil.androidify.R
+import kotlinx.android.synthetic.main.activity_nav_drawer.*
 
 class NavDrawerActivity : AppCompatActivity() {
 
@@ -10,5 +14,24 @@ class NavDrawerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nav_drawer)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        /**
+         * Add an [ActionBarDrawerToggle], which displays the hamburger icon in the toolbar.
+         */
+        val drawerToggle = ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close)
+        drawer.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                drawer.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
