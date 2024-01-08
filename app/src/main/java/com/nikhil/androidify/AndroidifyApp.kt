8c02661c6ed95dea413e76da2eb684f333b81270
Nikhil Mehta <nikhilmht33@gmail.com>
2024-01-09 00:51:45 +0530

    Remove WorkManager code from the app

--- app/src/main/java/com/nikhil/androidify/AndroidifyApp.kt
+++ app/src/main/java/com/nikhil/androidify/AndroidifyApp.kt
@@ -1,15 +1,9 @@
 package com.nikhil.androidify
 
 import android.app.Application
-import androidx.work.*
-import com.nikhil.androidify.codelabs.coroutines.main.RefreshMainDataWork
 import timber.log.Timber
-import java.util.concurrent.TimeUnit
 
-/**
- * Override application to setup background work via [WorkManager]
- */
-class AndroidifyApp : Application(), Configuration.Provider {
+class AndroidifyApp : Application() {
 
     /**
      * onCreate is called before the first screen is shown to the user.
@@ -20,43 +14,7 @@ class AndroidifyApp : Application(), Configuration.Provider {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
-        setupWorkManagerJob()
     }
 
-    /**
-     * Setup WorkManager background job to 'fetch' new network data daily.
-     */
-    private fun setupWorkManagerJob() {
-        // initialize WorkManager with a Factory
-//        val workManagerConfiguration = Configuration.Builder()
-//            .setWorkerFactory(RefreshMainDataWork.Factory())
-//            .build()
-//        WorkManager.initialize(this, workManagerConfiguration)
 
-        // Use constraints to require the work only run when the device is charging and the
-        // network is unmetered
-        val constraints = Constraints.Builder()
-            .setRequiresCharging(true)
-            .setRequiredNetworkType(NetworkType.UNMETERED)
-            .build()
-
-        // Specify that the work should attempt to run every day
-        val work = PeriodicWorkRequestBuilder<RefreshMainDataWork>(1, TimeUnit.DAYS)
-            .setConstraints(constraints)
-            .build()
-
-        // Enqueue it work WorkManager, keeping any previously scheduled jobs for the same
-        // work.
-        WorkManager.getInstance(this)
-            .enqueueUniquePeriodicWork(RefreshMainDataWork::class.java.name,
-                ExistingPeriodicWorkPolicy.KEEP, work)
-    }
-
-    override val workManagerConfiguration: Configuration
-        get() {
-            // initialize WorkManager with a Factory
-            return Configuration.Builder()
-                .setWorkerFactory(RefreshMainDataWork.Factory())
-                .build()
-        }
 }
